8bd90afc317abc438c85837f0b9798fcad813e18
ImDwOmer Ziauddin ansari <Omer.Ansari@satincreditcare.com>
2021-10-04 17:22:47 +0530

    second part

--- src/main/java/com/hql/HQLExample.java
+++ src/main/java/com/hql/HQLExample.java
@@ -11,37 +11,27 @@ import com.tut.Student;
 
 public class HQLExample {
 public static void main(String[] args) {
-	//1- create sessionfactory
-	//koi bhi data db se nikalne  liye through hibernate we require sessionfactory
-	//create sessionfactory
+	
 	Configuration cfg=new Configuration();
 	cfg.configure("hibernate.cfg.xml");
 	SessionFactory factory = cfg.buildSessionFactory();
 	
 	 //HQL
 	//Syntax
-	String query="from Student";  
-	//Student is entity class after firing this query hibernate will give us all object from the table student
 	
-	//String query="from Student where city='Lucknow' ";
-	
+	//A-->String query="from Student where city=:x ";
+	String query="from Student as s where s.city=: x and s.name=: n";
 	
 	
-	//2- we want session to fire query so getting session from factory
+	
 	Session s=factory.openSession();
-	
-	//3- s has method to fire queries so we will get object of Query
 	
 	Query q=s.createQuery(query);//now query went into Query object q
 	
+	//A-->q.setParameter("x", "Lucknow");//same result but dynamic value
+	q.setParameter("x", "Lucknow");
+	q.setParameter("n", "Peter");
 	
-	//Single (unique) if we want k our query bring unique result
-	//q.uniqueResult(); error if in db more than 1 result
-	System.out.println(q.setMaxResults(1).uniqueResult());
-	System.out.println("\n multiple results");
-	
-	
-	//Multiple (list of objects) if we want our query to bring all objects
 	List<Student> list=q.list();
 	
 	for(Student student : list)

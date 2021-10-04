ffbef71d5304b3f791d9567d72f98b8d70e8fefd
ImDwOmer Ziauddin ansari <Omer.Ansari@satincreditcare.com>
2021-10-05 01:08:13 +0530

    update query of HQL

--- src/main/java/com/hql/HQLExample.java
+++ src/main/java/com/hql/HQLExample.java
@@ -26,32 +26,46 @@ public static void main(String[] args) {
 	
 	
 	Session s=factory.openSession();
-	
-	Query q=s.createQuery(query);//now query went into Query object q
-	
-	//A-->q.setParameter("x", "Lucknow");//same result but dynamic value
-	q.setParameter("x", "Lucknow");
-	q.setParameter("n", "Peter");
-	
-	List<Student> list=q.list();
-	
-	for(Student student : list)
-	{
-		System.out.println(student.getName()+" course= "+student.getCerti().getCourse());
-	}
-	
-	
+//	
+//	Query q=s.createQuery(query);//now query went into Query object q
+//	
+//	//A-->q.setParameter("x", "Lucknow");//same result but dynamic value
+//	q.setParameter("x", "Lucknow");
+//	q.setParameter("n", "Peter");
+//	
+//	List<Student> list=q.list();
+//	
+//	for(Student student : list)
+//	{
+//		System.out.println(student.getName()+" course= "+student.getCerti().getCourse());
+//	}
+//	
+//	
 	System.out.println("-------------------------------------------------------------------");
 	Transaction tx=s.beginTransaction();
-	//using alias s not necessary 
-	Query q2=s.createQuery("delete from Student as s where s.city=: c");
 	
-	//firing Query
-	q2.setParameter("c", "ABC");
-	int r=q2.executeUpdate();//  if not begining Transaction then  javax.persistence.TransactionRequiredException
 	
-	System.out.println("Deleted");
-	System.out.println(r);
+//	//using alias s not necessary 
+//	Delete
+//	Query q2=s.createQuery("delete from Student as s where s.city=: c");
+//	
+//	//firing Query
+//	q2.setParameter("c", "ABC");
+//	int r=q2.executeUpdate();//  if not begining Transaction then  javax.persistence.TransactionRequiredException
+//	
+//	System.out.println("Deleted");
+//	System.out.println(r);
+	
+	//update query
+	Query q2=s.createQuery("update Student set city=: c where name=: n");
+	//Student is entity class ie persistant class and city name are variables of this class
+	q2.setParameter("c","Delhi");
+	q2.setParameter("n", "Peter");
+	int r=q2.executeUpdate();
+	
+	System.out.println(r+" objects updated");
+		
+	
 	
 	tx.commit();
 	
